f30d3970c792b2774fa77332d7d8ba7df43ebcf2
DoukheeWon <fain9301@yahoo.com>
2024-05-06 21:17:00 +0900

    feat: add syscalls

--- syscalls.c
+++ syscalls.c
@@ -3,9 +3,19 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
 #include "stm32f10x.h"
 
 #define  U1_BUFFER_SIZE  100
+
+register char *stack_ptr asm("sp");
+
+char *__env[1] = {0};
+char **environ = __env;
 
 //retargetting _write
 //해당 객체 또는 함수가 참조되지 않아도 사용하는 것처럼 컴파일러로 하여금 삭제되지 않도록 한다. __attribute__((used))
@@ -21,9 +31,16 @@ __attribute__ ((used)) int _write(int fd, char *ptr, int len) {
     return len;
 }
 
-__attribute__ ((used)) int _read(int fd, char *ptr, int len) {
-    while ((USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET));
-    return USART_ReceiveData(USART1);
+__attribute__((used)) int _read(int fd, char *ptr, int len) {
+    size_t i;
+    for (i = 0; i < len; ++i) {
+        while ((USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET));
+        *ptr++ = USART_ReceiveData(USART1);
+        if (*ptr == '\n') {
+            break;
+        }
+    }
+    return i;
 }
 
 /*
@@ -65,120 +82,99 @@ __attribute__ ((used)) int _read(int fd, char *ptr, int len)
 }
 */
 
-/*
-__attribute__ ((used)) int _read(int file, int ptr, int dir)
-{
-    #if 0
-        init i;
-        (void)file;
-
-        for(i = 0; i < len; i++)
-        {
-            *ptr++ = USART_ReceiveData(USART1);
-            while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
-        }
-    return len;
-}
-*/
-
 __attribute__ ((used)) int _close(int file) {
     return -1;
 }
 
-//retargetting _read
-/*
-__attribute__ ((used)) int _read(int fd, char *buf, int len)
-{
-    size_t i;
-    for(i = 0; i < len; i++)
-    {
-        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET);
-        USART_Receive(USART1);
-    }
-    return len;
-    for(fd = 0; fd < count; fd++)
-    {
-        while(pc_buffer_empty(&usart1_rx))
-        {
-            ;
-        }
-        disable_interruptes();
-        pc_buffer_remove(&uart1_rx, (char *)buf++);
-        enable_interrupts();
-    }
-    return count;
-//    size_t i;
-//    for(i = 0; i < len; ++i)
-//    {
-//        buf[i] = USART_ReceiveData(USART1);
-//        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == SET);
-//    }
-//    return i;
+__attribute__((weak)) int _getpid(void) {
+    return 1;
 }
-*/
-/*
 
-//retargetting _read
-__attribute__ ((used)) int _read(int __fd, void *__buf, size_t _nbyte)
-{
+__attribute__((weak)) int _kill(int pid, int sig) {
+    errno = EINVAL;
+    return -1;
+}
+
+__attribute__((weak)) void _exit(int status) {
+    _kill(status, -1);
+    while (1) {}        /* Make sure we hang here */
+}
+
+/** using malloc */
+__attribute__((weak)) caddr_t _sbrk(int incr) {
+    extern char end asm("end");
+    static char *heap_end;
+    char *prev_heap_end;
+
+    if (heap_end == 0)
+        heap_end = &end;
+
+    prev_heap_end = heap_end;
+    if (heap_end + incr > stack_ptr) {
+//		write(1, "Heap and stack collision\n", 25);
+//		abort();
+        errno = ENOMEM;
+        return (caddr_t) - 1;
+    }
+
+    heap_end += incr;
+
+    return (caddr_t) prev_heap_end;
+}
+
+__attribute__((weak)) int _close(int file) {
+    return -1;
+}
+
+
+__attribute__((weak)) int _fstat(int file, struct stat *st) {
+    st->st_mode = S_IFCHR;
     return 0;
 }
 
-int fgetc(FILE *f)
-{
-int c;
-
-while (__HAL_UART_GET_FLAG(&huart1, UART_FLAG_RXNE) == RESET);
-
-c = LL_USART_ReceiveData8(huart1.Instance);
-
-return fputc(c, f);
+__attribute__((weak)) int _isatty(int file) {
+    return 1;
 }
 
-*/
-
-
-/*
-//need to testing 
-__attribute__ ((used)) int _write(int fd, char *ptr, int len)
-{
-    size_t i;
-    for(i = 0; i < len; i++)
-    {
-        fputc(USART1, (uint8_t *)&ch);
-    }
-    return len;
+__attribute__((weak)) int _lseek(int file, int ptr, int dir) {
+    return 0;
 }
 
-*/
+__attribute__((weak)) int _open(char *path, int flags, ...) {
+    /* Pretend like we always fail */
+    return -1;
+}
 
-/*
-//int _read (int fd, const void *buf, size_t count)
-//{
-//  size_t CharCnt = 0x00;
- 
-//  (void)fd;                            /* Parameter is not used, suppress unused argument warning */
-//  for (;count &gt; 0x00; --count) {
-//    if ((UART0_PDD_ReadInterruptStatusReg(UART0_BASE_PTR)  UART0_S1_RDRF_MASK) == 0x00) { /* Any data in receiver buffer */
-/* Clear error flags */
-//     UART0_PDD_ClearInterruptFlags(UART0_BASE_PTR,0x1FU);
-//     if (CharCnt != 0x00) {           /* No, at least one char received? */
-//       break;                         /* Yes, return received char(s) */
-//     } else {                         /* Wait until a char is received */
-//       while ((UART0_PDD_ReadInterruptStatusReg(UART0_BASE_PTR)  UART0_S1_RDRF_MASK) == 0x00) {};
-//     }
-//   }
-//   CharCnt++;                         /* Increase char counter */
-//   /* Save character received by UARTx device into the receive buffer */
-//   *(uint8_t*)buf = (unsigned char)UART0_PDD_GetChar8(UART0_BASE_PTR);
-//   /* Stop reading if CR (Ox0D) character is received */
-//  if (*(uint8_t*)buf == 0x0DU) {     /* New line character (CR) received ? */
-//     *(uint8_t*)buf = '\n';           /* Yes, convert LF to '\n' char. */
-//     break;                           /* Stop loop and return received char(s) */
-//   }
-//   (uint8_t*)buf++;                   /* Increase buffer pointer */
-// }
-//return 1; /* WRONG! */
-// return CharCnt;
-//}
-//*/
+__attribute__((weak)) int _wait(int *status) {
+    errno = ECHILD;
+    return -1;
+}
+
+__attribute__((weak)) int _unlink(char *name) {
+    errno = ENOENT;
+    return -1;
+}
+
+__attribute__((weak)) int _times(struct tms *buf) {
+    return -1;
+}
+
+__attribute__((weak)) int _stat(char *file, struct stat *st) {
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+__attribute__((weak)) int _link(char *old, char *new) {
+    errno = EMLINK;
+    return -1;
+}
+
+__attribute__((weak)) int _fork(void) {
+    errno = EAGAIN;
+    return -1;
+}
+
+__attribute__((weak)) int _execve(char *name, char **argv, char **env) {
+    errno = ENOMEM;
+    return -1;
+}
